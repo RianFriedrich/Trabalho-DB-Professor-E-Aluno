6c3c5b43c30fc1654b6bd4dd14a844a4107d23b8
RianFriedrich <125458446+RianFriedrich@users.noreply.github.com>
2024-03-27 14:17:49 -0300

    Add files via upload

---
+++ TrabalhoDBProfessorEAluno/src/Usuario/Usuario.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Usuario;
+
+/**
+ *
+ * @author user
+ */
+public class Usuario {
+    private String nome;
+    private String sexo;
+    private int CPF;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+    
+    
+    
+}
